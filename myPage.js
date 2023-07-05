5a7d2520953dd7e1a5448a71c980efde9c343b7b
Maneesha <amaneesha@gmail.com>
2023-07-04 22:29:25 -0500

    First commit

---
+++ myPage.js
@@ -0,0 +1,15 @@
+function myFunction(id) {
+  let varName = id;
+
+  if (varName == 1) {
+    document.location.href = "tropicalFlowering.html";
+    
+  }
+  else if (varName == 2) {
+    document.location.href = "annualFlowering.html";
+    
+  }
+}
+function mysignUp() {
+  alert("Submitted successfully!");
+}
